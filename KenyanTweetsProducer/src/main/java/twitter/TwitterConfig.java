6172e36c7bdc5de8608caa5b14bdfec21e5fc1ba
Johnstone Ananda <johnolwamba@gmail.com>
2022-09-26 11:48:42 -0500

    remove commented code

--- KenyanTweetsProducer/src/main/java/twitter/TwitterConfig.java
+++ KenyanTweetsProducer/src/main/java/twitter/TwitterConfig.java
@@ -7,21 +7,3 @@ public class TwitterConfig {
     public static final String TOKEN = "1050212468-KDgBiebSAYUkxwbLp5y70n10cwFYSch4EkXove6";
     public static final String BEARERTOKEN = "AAAAAAAAAAAAAAAAAAAAAO2NhQEAAAAA%2F4RZhZRaFvuq5nZ%2B2fX905YBbCY%3DDW4ku9JEkJH93AI1CKwTOSDHaEvh3hQHObSkeNh6G6KhGwdefd";
 }
-
-/*
-
-API Key
-ubPibRQKysM5SnqGRnxsWNN9Z
- 
-API Key Secret
-l0KDoQuOsAU6j76wUZN00ngy3g8lpo5MIytJEhWuuC4VNJLV9f
- 
-Bearer Token
-AAAAAAAAAAAAAAAAAAAAAO2NhQEAAAAA%2F4RZhZRaFvuq5nZ%2B2fX905YBbCY%3DDW4ku9JEkJH93AI1CKwTOSDHaEvh3hQHObSkeNh6G6KhGwdefd
- 
-Access Token:
-1050212468-KDgBiebSAYUkxwbLp5y70n10cwFYSch4EkXove6
- 
-Access Token Secret
-Qnw4OXBLOIgKFUWnKPdUa8jLr71sS84mkHAus1XFiJskU
-*/
